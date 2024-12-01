d33a88ad7af583c32f15a82011da46004fd1200b
Daniel Gahler <darkshoxx@yahoo.de>
2024-12-01 21:35:47 +0000

    C done

--- 2024/1/c_hack.c
+++ 2024/1/c_hack.c
@@ -4,6 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int smallest_difference(int left_array[1000], int right_array[1000] ){
+    int min_left = 100000;
+    int min_index_left = 0;
+    int min_right = 100000;
+    int min_index_right = 0;
+    for(int search_index = 0; search_index < 1000; search_index++){
+        if(left_array[search_index]< min_left){
+            min_left = left_array[search_index];
+            min_index_left = search_index;
+        }
+        if(right_array[search_index]< min_right){
+            min_right = right_array[search_index];
+            min_index_right = search_index;
+        }
+    }
+    left_array[min_index_left] = 100000;
+    right_array[min_index_right] = 100000;
+        if (min_left > min_right){
+        return min_left - min_right;
+    } else {
+        return min_right - min_left;
+    }
+}
+
 int main() {
 
     // writing print statement to print hello world
@@ -15,8 +39,8 @@ int main() {
     }
     // option: load file into buffer line by line
     char buffer[14];
-    int left_array[100];
-    int right_array[100];
+    int left_array[1000];
+    int right_array[1000];
     int index = 0;
 
 
@@ -31,18 +55,18 @@ int main() {
             index = index + 1;
         }
     }
-    int min = 0
-    int min_index = 0
-    for(int search_index = 0; search_index < 100; search_index++){
-        if(left_array[print_index]> min){
-            min = left_array[print_index]
-            min_index = print_index
-        }
+    int accumulator = 0;
+    for(int i=0; i<1000; i++){
+    accumulator += smallest_difference(left_array, right_array);
     }
-    for (int print_index=0; print_index<100; print_index++){
+
+    for (int print_index=0; print_index<1000; print_index++){
         printf("Leftarray entry %d is %d\n", print_index, left_array[print_index]);
 
     }
+
+    //printf("Left min is %d, right min is %d\n", min_left, min_right);
+    printf("Solution is %d\n", accumulator);
     // option: load entire file into buffer
 
     // char* buffer = NULL;
