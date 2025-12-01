79130685ab5f0366312be75eddddb7b1775b966d
Daniel Gahler <daniel.gahler@strath.ac.uk>
2025-12-01 12:07:16 +0000

    Day 1 done

---
+++ 2025/1/solve.py
@@ -0,0 +1,109 @@
+import os
+HERE = os.path.abspath(os.path.dirname(__file__))
+INPUT = os.path.join(HERE, "input.txt")
+INPUTDG = os.path.join(HERE, "inputdg.txt")
+INPUTTEST = os.path.join(HERE, "test.txt")
+
+with open(INPUTDG, "r") as f:
+    content = f.readlines()
+
+directions = []
+distances = []
+
+# Part 1
+for line in content:
+    if line:
+        directions.append(line[0])
+        distances.append(int(line[1:]))
+
+# print(directions)
+# print(distances)
+
+start = 50
+counter = 0
+for dir, dist in zip(directions, distances):
+    if dir == "L":
+        start = (start + dist) % 100
+    else:
+        start = (start - dist) % 100
+    if start == 0:
+        counter += 1
+print("Counter:",counter)
+
+# # Part 2
+
+start = 50
+counter = 0
+for dir, dist in zip(directions, distances):
+    lagged_start = start
+    if dir == "L":
+        start = (start - dist) 
+    else:
+        start = (start + dist)
+    if start >= 100:
+        counter += start // 100
+    elif start == 0:
+        counter +=1
+    elif start < 0 and lagged_start !=0:
+        counter += abs(start // 100)
+        if start % 100 == 0:
+            counter += 1 # what an obscure edge case.
+    elif start < 0 and lagged_start == 0:
+        counter += abs(start // 100) - 1
+    start = start % 100
+print("Counter:", counter)
+
+# Part 2 alt:
+start = 50
+counter = 0
+for dir, dist in zip(directions, distances):
+    while dist > 0:
+        if dir == "L":
+            start -= 1
+        else:
+            start += 1
+        start = start % 100
+        if start == 0:
+            counter +=1
+        dist -=1
+print("Counter:", counter)
+
+import copy
+# Part 2 comparer:
+start_check = 50
+start_run = 50
+counter_check = 0
+counter_run = 0
+for dir, dist in zip(directions, distances):
+    # RUN
+    dist_run = copy.copy(dist)
+
+    while dist_run > 0:
+        if dir == "L":
+            start_run -= 1
+        else:
+            start_run += 1
+        start_run = start_run % 100
+        if start_run == 0:
+            counter_run +=1
+        dist_run -=1
+    # CHECK
+    lagged_start = start_check
+    if dir == "L":
+        start_check = (start_check - dist) 
+    else:
+        start_check = (start_check + dist)
+    if start_check >= 100:
+        counter_check += start_check // 100
+    elif start_check == 0:
+        counter_check +=1
+    elif start_check < 0 and lagged_start !=0:
+        counter_check += abs(start_check // 100)
+        if start_check % 100 == 0:
+            counter_check += 1
+    elif start_check < 0 and lagged_start == 0:
+        counter_check += abs(start_check // 100) - 1
+    start_check = start_check % 100    
+
+    if counter_check != counter_run:
+        print("HERE")
---
+++ MiniAOC/Template.py
@@ -0,0 +1,43 @@
+# The pathlib library has a class called Path that is very helpful
+# for navigating fodlers and files, so we import it.
+from pathlib import Path
+
+# This command will generate an object for the folder this very file is in.
+HERE = Path(__file__).parent
+
+# This command will generate an object for the input file, by combining the 
+# path the file is on and the name of the file itself.
+# PLEASE RENAME <FILENAME> to your puzzle input file name 
+# AND ensure they're in the same folder.
+INPUT = HERE / "<FILENAME>.txt"
+
+# To read the file, there are multiple ways, but the standard, safe way
+# is as follows. You don't need to know how it works, but if you wish to 
+# look it up, it's called a context manager. The "r" stands for "read" as we
+# only want to know what's in the file, not change it.
+with open(INPUT, "r") as f:
+
+    # we can create a string that contains the entire file, including linebreaks:
+    file_content = f.read()
+
+    # (ignore, we're returning the pointer to the beginning of the file for
+    # the second pass)
+    f.seek(0)
+
+    # or we can create a list that contains all the lines in the file. This
+    # will make it easier to iterate through those lines, so that's what
+    # I'd recommend.
+    file_lines = f.readlines()
+
+# note that when we go back to the base indentation level, the f object will
+# be out of scope, so f.read will no longer work here.
+
+# some cleanup you may ignore
+file_lines = [line[:-1] for line in file_lines if line[-1]=="\n"]
+
+# now your inputs are in file_content
+print(file_content)
+# and file_lines
+print(file_lines)
+
+
