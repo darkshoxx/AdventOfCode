a2d1eac6c4dc470fd223905fbc286d9ae1f0a7d9
Daniel Gahler <daniel.gahler@strath.ac.uk>
2025-12-05 11:28:38 +0000

    D5 Part 2 +shame

--- 2025/5/solve.py
+++ 2025/5/solve.py
@@ -7,6 +7,8 @@ with open(INPUT, "r") as f:
     content = f.readlines()
 cleanlines = [line[:-1] for line in content]
 
+# Part 1
+
 ranger = True
 ranges = []
 candidates = []
@@ -31,3 +33,62 @@ for candidate in candidates:
                 accumulator +=1
                 found = True
 print(accumulator)
+
+# Part 2
+total = len(ranges)
+index_sets_dict = {index:set() for index in range(len(ranges))}
+for base_index, (base_first, base_last) in enumerate(ranges):
+    for test_index, (test_first, test_last) in enumerate(ranges):
+        if base_first<=test_first<=base_last or base_first<=test_last<=base_last:
+            index_sets_dict[base_index].add(test_index)
+            # for index in index_sets_dict[test_index]:
+            #     index_sets_dict[index].add(base_index)
+
+# I comment out the brute force of shame, but
+# it shall be on display as a memorial 
+# for _ in range(300): 
+for index, value_set in index_sets_dict.items():
+    processing_set = set([index])
+    processing = True
+    while processing:
+        processing = False
+        for item in value_set.difference(processing_set):
+            processing_set.add(item)
+            processing = True
+            for value in index_sets_dict[item]:
+                processing_set.add(value)
+    for item in value_set:
+        index_sets_dict[item] = processing_set
+
+
+# Test that for every index, for every item in it's value set has the value set as value set
+trues = 0
+falses = 0
+for index, value_set in index_sets_dict.items():
+    for item in value_set:
+        if index_sets_dict[item] == value_set:
+            trues +=1
+        else:
+            falses +=1
+
+print("Trues:  ", trues)
+print("Falses: ", falses)
+
+# print(index_sets_dict)
+indices_used = []
+final_ranges = []
+for key, value in index_sets_dict.items():
+    if key not in indices_used:
+        indices_used += list(value)
+        lower = min([ranges[item][0] for item in value])
+        upper = max([ranges[item][1] for item in value])
+        final_ranges.append((lower, upper))
+# print(final_ranges)
+# print(len(final_ranges))
+
+accumulator = 0
+for first, last in final_ranges:
+    accumulator += last - first +1
+
+print("current:  ", accumulator)
+print("previous: ", 342018167474526)
